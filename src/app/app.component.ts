1b7ea049dff93c498287c75a93798f7fffa8f4eb
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-01 11:48:25 +0530

    feat-tutorial one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
+  public msg = '';
+
+  onChange(val : any){
+    this.msg=val;
+  }
+
+  public fruits = [
+    {name : "Apple", price : 85},
+    {name : "Kiwi", price : 210},
+    {name : "Orange", price : 25},
+    {name : "Mango", price : 50},
+    {name : "Grapes", price : 120}
+  ]
 }
