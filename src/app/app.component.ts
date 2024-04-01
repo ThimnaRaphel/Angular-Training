28b043e09b0dc368606996959292352047b70a83
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-01 15:55:19 +0530

    feat-reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,100 @@
+
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient,  HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HttpClientModule,FormsModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
-  public msg = '';
-  public data : any;
-  user : any={
+  title = 'MyFirstAngularApp';
+ 
+ 
+    //-----------------------TWO WAY RENDERING-----------------------
+  public msg = 'Hello World';
+ 
+  onchange(val : any) {
+    this.msg = val;
+  }
+ 
+  //-----------------------TABLE RENDERING-----------------------
+ 
+  public fruits = [
+    {name : 'Apple', price : 50},
+    {name : 'Apple', price : 80},
+    {name : 'Apple', price : 120},
+    {name : 'Apple', price : 150},
+    {name : 'Apple', price : 20},
+  ]
+ 
+ 
+  //-----------------------APIs-----------------------
+ 
+  // public data : any;  //For API data
+  // constructor(private http : HttpClient) {}
+  // ngOnInit() {
+  //   this.fetchDetails();
+  // }
+ 
+  // fetchDetails() {
+  //   this.http.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(
+  //     (resp : any) => {
+  //       console.log('Response is : ',resp);
+  //       this.data = resp;
+  //     }
+  //   )
+  // }
+ 
+ 
+  //-----------------------TEMPLATE DRIVEN FORMS-----------------------
+  user : any = {
     firstName : '',
     lastName : '',
   }
+ 
+  // onSubmit() {
+  //   console.log("User ---->", this.user);
+  // }
+ 
+ 
+    //-----------------------REACTIVE FORMS-----------------------
+ 
+    constructor(private fb : FormBuilder) {}
+ 
+    submit = false;
+    registrationForm = this.fb.group ({
+      firstName : ['',Validators.required],
+      lastName : [],
+      phone : [''],
+      email : ['', [Validators.email,this.emailValidator]]
+    })
+ 
+    get f() {
+      return this.registrationForm.controls;
+    }
+ 
+    ngOnInit() {}
+ 
+    onSubmit() {
+      this.submit = true;
+      console.log("Clicked..!");
+      console.log(this.registrationForm.value);
+      console.log(this.registrationForm.controls);
+    }
 
-  constructor(private http:HttpClient){}
-  ngOnInit(){
-    this.fetchDetails();
-  }
-
-
-  onChange(val : any){
-    this.msg=val;
-  }
-
-  public fruits = [
-    {name : "Apple", price : 85},
-    {name : "Kiwi", price : 210},
-    {name : "Orange", price : 25},
-    {name : "Mango", price : 50},
-    {name : "Grapes", price : 120}
-  ]
-   fetchDetails=()=>{
-    this.http.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(
-      (resp : any)=>{
-        console.log(resp);
-        this.data= resp;
+    emailValidator(control : any){
+      const emailRegex = /^\w+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$/;
+      if(!control.value || emailRegex.test(control.value)){
+        return null;
       }
-    )
-  }
-
-  onSubmit(){
-    console.log(this.user);
-  }
+      else{
+        return {invalidEmail : true};
+      }
+    }
 }
+ 
