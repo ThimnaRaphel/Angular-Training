2faeba26bb63f01c5d46c9b5e275dd4e286a036f
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-01 12:58:33 +0530

    feat-api and forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient,  HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
   public msg = '';
+  public data : any;
+  user : any={
+    firstName : '',
+    lastName : '',
+  }
+
+  constructor(private http:HttpClient){}
+  ngOnInit(){
+    this.fetchDetails();
+  }
+
 
   onChange(val : any){
     this.msg=val;
@@ -24,4 +37,16 @@ export class AppComponent {
     {name : "Mango", price : 50},
     {name : "Grapes", price : 120}
   ]
+   fetchDetails=()=>{
+    this.http.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (resp : any)=>{
+        console.log(resp);
+        this.data= resp;
+      }
+    )
+  }
+
+  onSubmit(){
+    console.log(this.user);
+  }
 }
